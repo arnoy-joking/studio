32ab9d981a30349f6609fde12012eaa374598071
Arnoy Goswami <arnoy799@gmail.com>
2025-06-27 14:48:35 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/class/[slug]/page.tsx
+++
@@ -1,2 +0,0 @@
-// This file is no longer used. The active page is now at src/app/(app)/class/[slug]/page.tsx
-export {};
--- src/app/dashboard/layout.tsx
+++
@@ -1,2 +0,0 @@
-// This file is no longer used. The active layout is now at src/app/(app)/layout.tsx
-export {};
